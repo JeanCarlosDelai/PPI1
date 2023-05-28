43d15368aa3b9ffb6a3ca173394871a386e65b34
JeanCarlosDelai <Jean@vieceli@outlook.com>
2023-05-28 16:32:38 -0300

    Finalização do modulo sobre Migrations

---
+++ aula-sequelize/seeders/20230528192910-demo-user.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert("Users", [
+      {
+        firstName: "John",
+        lastName: "Doe",
+        email: "example@example.com",
+        bio: "Bla bla bla bla bla bla",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Users", null, {});
+  },
+};
